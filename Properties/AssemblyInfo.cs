29d23848f9858013d581695e294e2ff918579447
iswenzz <suxlolz1528@gmail.com>
2020-01-30 20:06:08 +0100

    Updated .csproj and packages.config

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Aiondatabase Parser")]
-[assembly: AssemblyDescription("Parse NPC drops from https://aiondatabase.net")]
+[assembly: AssemblyTitle("AION Database Parser")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("https://iswenzz.com")]
-[assembly: AssemblyProduct("Aiondatabase Parser")]
-[assembly: AssemblyCopyright("Copyright © Iswenzz 2018-2019")]
+[assembly: AssemblyProduct("AION Database Parser")]
+[assembly: AssemblyCopyright("Copyright © Iswenzz 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ccd07efd-e66c-49a0-9aea-95ff81600e94")]
+[assembly: Guid("e11bbe15-6eaa-4948-9dea-f069a867c10b")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
